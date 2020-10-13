f603de2024d5a35e8ca5dbcbf178de7061385a17
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-13 21:46:10 +0530

    Added rootquery to give author list

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -86,6 +86,12 @@ const RootQuery = new GraphQLObjectType({
         return books;
       },
     },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
+      },
+    },
   },
 });
 
