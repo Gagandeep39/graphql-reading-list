8757ad2f3c64e761d26d0d597d62a0198d2c2cdb
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-13 12:24:51 +0000

    Added graphql, express-graphql dependency

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
 const app = express();
+
+app.use('/graphql', graphqlHTTP({}));
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
