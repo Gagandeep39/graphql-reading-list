a143b3fdc04b196cc00acddc5d495f94cb7f2ca8
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-14 01:27:12 +0530

    Added apollo, dotenv dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
+// require('dotenv').config();
+import { ApolloProvider, ApolloClient } from '@apollo/client';
 import BookList from './components/BookList';
+
+// Apollo Init
+
+const client = new ApolloClient({
+  uri: process.env.SERVER_URI,
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Reading List
-      </h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div className='App'>
+        <h1>Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
