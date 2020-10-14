2bb0b64ca3c403902f02248b151d4c0aa3cd2506
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-14 11:58:36 +0530

    Added basic validation

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -10,6 +10,15 @@ import { graphql } from '@apollo/client/react/hoc';
 import { getAuthorsQuery } from '../graphql/queries';
 
 class AddBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      genre: '',
+      authorId: '',
+    };
+  }
+
   displayAuthors() {
     const data = this.props.data;
     if (data.loading) return <option disabled>Loading...</option>;
@@ -21,20 +30,35 @@ class AddBook extends Component {
       ));
   }
 
+  submitForm(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <form id='add-book' onSubmit={(e)=>{e.preventDefault()}}>
+      <form id='add-book' onSubmit={this.submitForm.bind(this)}>
         <div className='field'>
           <label>Book name</label>
-          <input type='text' />
+          <input
+            required
+            type='text'
+            value={this.state.name}
+            onChange={(e) => this.setState({ name: e.target.value })}
+          />
         </div>
         <div className='field'>
           <label>Genre</label>
-          <input type='text' />
+          <input
+            required
+            type='text'
+            value={this.state.genre}
+            onChange={(e) => this.setState({ genre: e.target.value })}
+          />
         </div>
         <div className='field'>
           <label>Author</label>
-          <select>
+          <select onChange={(e) => this.setState({ authorId: e.target.value })}>
             <option>Select Author</option>
             {this.displayAuthors()}
           </select>
