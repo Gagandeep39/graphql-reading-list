96942c38fb9ac34fdc501654f0c4298bb90af851
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-14 12:06:59 +0530

    Created addBook mutation

---
+++ client/src/graphql/mutations.js
@@ -0,0 +1,19 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-14 12:00:00
+ * @modify date 2020-10-14 12:00:00
+ * @desc GraphQL Mutations
+ */
+import { gql } from '@apollo/client';
+
+const addBookMutation = gql`
+  mutation {
+    addBook (name = "", genre = "", authorId = "") {
+      name
+      id
+    }
+  }
+`;
+
+export { addBookMutation };
